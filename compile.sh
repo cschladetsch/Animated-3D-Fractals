c197886bd5ed2c91ad357100fee8a333f9a6907b
Christian Schladetsch <christian.schladetsch@gmail.com>
2016-04-10 23:59:20 +1000

    now links with OpenGL and C++ libs - now for rest for project

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-gcc -o foo boxplorer2.cc -g -O2 -D_REENTRANT -I/usr/local/include/SDL2 -DHAVE_OPENGL -g -lSDL2_test -L/usr/local/lib -Wl,-rpath,/usr/local/lib -lSDL2 -IAntTweakBar/include -Iinclude -std=c++11
+g++ -o foo boxplorer2.cc -g -std=c++11 -O2 -D_REENTRANT -I/usr/local/include/SDL2 -IAntTweakBar/include -Iinclude -DHAVE_OPENGL -g -lSDL2_test -L/usr/local/lib -Wl,-rpath,/usr/local/lib -lSDL2 -lstdc++  -lGL -lm
