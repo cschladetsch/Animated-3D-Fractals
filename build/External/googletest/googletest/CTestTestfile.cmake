4c2165405a65fae4e452939d79c326aabfcd035f
Christian <christian.schladetsch@gmail.com>
2026-09-18 23:40:48 +1000

    Clean:

--- build/External/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/christian/local/repos/Animated-3D-Fractals/External/googletest/googletest
-# Build directory: /home/christian/local/repos/Animated-3D-Fractals/build/External/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
